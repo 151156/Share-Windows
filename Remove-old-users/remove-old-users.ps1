5dd3a81ad834b16c8ed251e8a547805b85f52dae
151156 <1511515.e68@gmail.com>
2026-08-20 10:36:47 +0200

    Add files via upload

---
+++ Remove-old-users/remove-old-users.ps1
@@ -0,0 +1,18 @@
+﻿# Days (example: 365 = 1 year)
+$thresholdDays = 180
+$limit = (Get-Date).AddDays(-$thresholdDays)
+
+Write-Host "== Remove old profiles =="
+
+Get-CimInstance Win32_UserProfile | Where-Object {
+    $_.Special -eq $false -and
+    $_.Loaded -eq $false -and
+    $_.LastUseTime -lt $limit
+} | ForEach-Object {
+    try {
+        Write-Host "Deleting profile: $($_.LocalPath)"
+        Remove-CimInstance $_
+    } catch {
+        Write-Host "ERROR: $($_.LocalPath)"
+    }
+}
---
+++ Remove-old-users/task-create-remove-users.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+REM --- Task name ---
+set TaskName=Remove-old-users
+
+REM --- Batch file location ---
+set ScriptPath=C:\temp\remove-users.bat
+
+REM --- Create task ---
+schtasks /create /tn "%TaskName%" /tr "\"%ScriptPath%\"" /sc onlogon /ru "SYSTEM" /rl HIGHEST /f
+
+echo Task create done: %TaskName%
+
